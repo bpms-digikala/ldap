33b7cb37c5bdaa6d70c8bbf5d35bb78ea249fc2c
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-25 00:18:37 +0330

    edit des

--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -180,9 +180,7 @@ USE_TZ = True
  
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIR= os.path.join(BASE_DIR,'static')
   
 
 # Extra places for collectstatic to find static files.
