48f5129dba655ef43e233192471dd5c5e5ce42b5
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-25 00:14:22 +0330

    from btre static files

--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -178,11 +178,11 @@ USE_TZ = True
  
 
  
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIR = '/static/'
-
-STATIC_ROOT = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'),
+]
   
 
 # Extra places for collectstatic to find static files.
