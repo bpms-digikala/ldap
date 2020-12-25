37ce2b0cc819084445ae5bb94d6ba57d26e6d11c
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-25 17:23:25 +0330

    push real directory in settings

--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/opt/Onboarding/onboarding/static/'
 
 
 # Extra places for collectstatic to find static files.
