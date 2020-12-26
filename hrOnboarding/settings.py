c7bfb578605914c1276e7bde1624600a13707396
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-26 23:34:50 +0330

    white noise 2th

--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -187,7 +187,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
