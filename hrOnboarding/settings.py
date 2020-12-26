8e9b8afa14662fda03f2cb11f16dd6940dae0aa4
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-26 21:34:29 +0330

    change dir static

--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -180,11 +180,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-
-STATIC_ROOT = '/opt/Onboarding/onboarding/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/Onboarding/onboarding/static/'
 
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
