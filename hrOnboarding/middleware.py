e01b1b5e3c21ab15b2af1d756c0eb4202c120d7d
Alireza Nabavieh <nabavieh@gmail.com>
2020-12-28 00:24:48 +0330

    correct middle"

--- hrOnboarding/middleware.py
+++ hrOnboarding/middleware.py
@@ -1,5 +1,8 @@
 
-class RemoveHeaders(object):
+from django.utils.deprecation import MiddlewareMixin
+
+
+class RemoveHeaders(MiddlewareMixin):
     def process_response(self, request, response):
-        response['Server'] = ''
+        del response['Server']
         return response
--- hrOnboarding/settings.py
+++ hrOnboarding/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 
 #  develop added by nabavieh   'corsheaders.middleware.CorsMiddleware',
 MIDDLEWARE = [
+    'hrOnboarding.middleware.RemoveHeaders',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'hrOnboarding.middleware.RemoveHeaders',
 ]
 
 #  develop added by nabavieh
